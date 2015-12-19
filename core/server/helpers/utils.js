dfc16e41433fb5ed82557cdab0d213f2ddc83a2d
spacerockzero <janderson@familysearch.org>
2015-12-18 20:55:05 -0700

    removed asset version hashes

--- core/server/helpers/utils.js
+++ core/server/helpers/utils.js
@@ -2,9 +2,9 @@ var _       = require('lodash'),
     utils;
 
 utils = {
-    assetTemplate: _.template('<%= source %>?v=<%= version %>'),
+    assetTemplate: _.template('<%= source %>'),
     linkTemplate: _.template('<a href="<%= url %>"><%= text %></a>'),
-    scriptTemplate: _.template('<script src="<%= source %>?v=<%= version %>"></script>'),
+    scriptTemplate: _.template('<script src="<%= source %>"></script>'),
     inputTemplate: _.template('<input class="<%= className %>" type="<%= type %>" name="<%= name %>" <%= extras %> />'),
     isProduction: process.env.NODE_ENV === 'production'
 };
